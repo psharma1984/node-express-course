09bc5f3a4273fb6ea3031f723c4191ce0f7dbdfc
psharma1984 <psharma.cad@gmail.com>
2023-11-02 12:16:11 -0700

    answers week6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,19 +3,21 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorhandlerMidddleware = require('./middleware/error-handler')
 
 //middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 //routes
-app.get('/hello', (req, res) => {
-    res.send('Task Manager App')
-})
 
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+app.use(errorhandlerMidddleware)
 
 
-const port = 3000
+const port = process.env.PORT || 3000
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI);
